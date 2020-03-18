87b0edc955eb93c70163a06febeaf728cd9ff968
Tawfiq <tawfiq.m@hotmail.fr>
2020-03-18 19:24:29 +0100

    init projet

---
+++ src/test/java/com/tmo/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
